79e98736662057eba6958ae208c4bdb092971937
oosha <oosha.github@gmail.com>
2018-10-19 18:16:52 +0900

    feat(web.routing): Add routing to app and dashboard.

--- client/web/src/app/app-routing.module.ts
+++ client/web/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { NavigationComponent } from './shared/navigation/navigation.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    component: NavigationComponent,
+    loadChildren: './dashboard/dashboard.module#DashboardModule',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
